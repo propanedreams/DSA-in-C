60181882abc998ebdf9704ec5d2b9493822d1e19
propanedream <eaaadey@students.eaaa.dk>
2024-11-11 20:51:50 +0100

    ændret bogosort til at tage tid og tælle shuffles

--- Sorts/bogosort/bogosort.c
+++ Sorts/bogosort/bogosort.c
@@ -13,10 +13,10 @@ bool isSorted(int arr[], int n) {
     return true;
 }
 
-// Function to shuffle the array
+// Shuffling function using Fisher-Yates algorithm 
 void shuffle(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        int randomIndex = rand() % n;
+    for (int i = n - 1; i > 0; i--) {
+        int randomIndex = rand() % (i + 1);
         // Swap arr[i] and arr[randomIndex]
         int temp = arr[i];
         arr[i] = arr[randomIndex];
@@ -25,11 +25,14 @@ void shuffle(int arr[], int n) {
 }
 
 // Bogo Sort function
-void bogoSort(int arr[], int n) {
+int bogoSort(int arr[], int n) {
+    int shuffle_count = 0;
     // Keep shuffling until the array is sorted
     while (!isSorted(arr, n)) {
         shuffle(arr, n);
+        shuffle_count++; // Increment the count for each shuffle
     }
+    return shuffle_count; // Return the total number of shuffles
 }
 
 void printArray(int arr[], int n) {
@@ -40,7 +43,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[10] = {3, 5, 1, 4, 2,7,6,9,8,10};
+    int arr[10] = {3, 5, 1, 4, 2, 7, 6, 9, 8, 10};
     int n = 10;
 
     // Seed random number generator
@@ -49,10 +52,19 @@ int main() {
     printf("Unsorted array:\n");
     printArray(arr, n);
 
-    bogoSort(arr, n);
+    // Start timing
+    clock_t start = clock();
+
+    // Perform Bogo Sort and get the shuffle count
+    int shuffle_count = bogoSort(arr, n);
+
+    // Stop timing
+    clock_t end = clock();
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
 
     printf("Sorted array:\n");
     printArray(arr, n);
+    printf("Bogo Sort took %f seconds and %d shuffles to sort the array.\n", time_taken, shuffle_count);
 
     return 0;
 }
